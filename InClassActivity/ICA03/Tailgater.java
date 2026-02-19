93be271066e139f6d06c88ed47768b8df05ee89c
mayayr115 <mayaramkishuncsh2027@gmail.com>
2026-02-18 23:41:50 -0500

    Completed Tailgater class

--- InClassActivity/ICA03/Tailgater.java
+++ InClassActivity/ICA03/Tailgater.java
@@ -10,34 +10,48 @@ public class Tailgater {
   public Tailgater(String name, boolean isBingFan) {
     this.name = name;
     this.isBingFan = isBingFan;
+    numberOfTailgaters += 1;
+
+    if (!isBingFan()) {
+      for (int i = 0; i < 5; i++) {
+        System.out.println("Go Binghamton!!!!!");
+      }
+    }
   }
 
   public String getName() {
     return this.name;
   }
 
-  public int getNumberOfTailgaters() {
+  public static int getNumberOfTailgaters() {
     return Tailgater.numberOfTailgaters;
   }
 
-  public boolean isBingFan() {
+  private boolean isBingFan() {
     return this.isBingFan;
   }
 
   public void getDrinkFromCooler() {
-    
+    if (!Tailgater.isCoolerEmpty()) {
+      this.drinks += 1;
+      Tailgater.drinksInCooler -= 1;
+      System.out.printf("%s grabbed a drink. Drinks taken: %d%n", this.name, this.drinks);
+      System.out.printf("There are %d more drinks in the cooler%n", Tailgater.drinksInCooler);
+    }
+    else System.out.printf("I'm sorry, %s. There are no drinks left%n", this.name);
   }
 
-  public boolean isCoolerEmpty() {
+  public static boolean isCoolerEmpty() {
     return Tailgater.drinksInCooler <= 0;
   }
 
-  public int getDrinksInCooler() {
+  public static int getDrinksInCooler() {
     return Tailgater.drinksInCooler;
   }
 
-  public void addDrinksToCooler(int drinks) {
-    this.drinks += drinks;
-    System.out.printf("Total drinks in the cooler: %d", drinks);
+  public static void addDrinksToCooler(int drinks) {
+    Tailgater.drinksInCooler += drinks;
+    System.out.printf("Added %d drinks to the cooler.%n", drinks);
+    System.out.printf("Total drinks in the cooler: %d%n", Tailgater.drinksInCooler);
   }
 }
