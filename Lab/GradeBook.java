3cce9ef4595e25285524c5c8fa6246f03c156a26
mayayr115 <mayaramkishuncsh2027@gmail.com>
2026-02-25 01:43:07 -0500

    Working on constructors for Gradebook class

--- Lab/GradeBook.java
+++ Lab/GradeBook.java
@@ -1,7 +1,16 @@
 package Lab;
 
 public class GradeBook {
-  public static void main (String[] args) {
-    
+  private double[] grades;
+
+  public GradeBook() {
+    this.grades = new double[0];
+  }
+
+  public GradeBook(GradeBook otherGradeBook) {
+    this.grades = new double[otherGradeBook.grades.length];
+    for (int i = 0; i < grades.length; i++) {
+      this.grades[i] = grades[i];
+    }
   }
 }
