bcbcbe2ebaa939f7ff2f19009c62c46a3577b4dd
mayayr115 <mayaramkishuncsh2027@gmail.com>
2026-02-27 21:33:09 -0500

    Set up methods

--- Lab/Lab04/GradeBook.java
+++ Lab/Lab04/GradeBook.java
@@ -13,4 +13,66 @@ public class GradeBook {
       this.grades[i] = grades[i];
     }
   }
+
+  public GradeBook(double[] newGrades) {
+
+  }
+
+  public GradeBook(float[] newGrades) {
+    
+  }
+
+  public GradeBook(int[] newGrades) {
+    
+  }
+
+  public GradeBook(long[] newGrades) {
+    
+  }
+
+  public void setGrades(float[] newGrades) {
+    
+  }
+
+  public void setGrades(double[] newGrades) {
+    
+  }
+
+  public void setGrades(int[] newGrades) {
+    
+  }
+
+  public void setGrades(long[] newGrades) {
+    
+  }
+
+  public double[] getGrades() {
+    return grades;
+  }
+
+  public boolean isEmpty() {
+    return grades.length <= 0;
+  }
+
+  public String toString() {
+    // String strGrades = (String) grades;
+    // return strGrades;
+    return " ";
+  }
+
+  public double minGrade() {
+    return 0.0;
+  }
+
+  public double maxGrade() {
+    return 0.0;
+  }
+
+  public double averageGrade() {
+    return 0.0;
+  }
+
+  public double modeGrade() {
+    return 0.0;
+  }
 }
