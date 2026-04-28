33b1327f543a145488f84f83c468b5a14f140fc4
mayayr115 <mayaramkishuncsh2027@gmail.com>
2026-04-28 10:27:23 -0400

    Implemented try/catch in code as required

--- InClassActivity/ICA05/GuessTheNumber.java
+++ InClassActivity/ICA05/GuessTheNumber.java
@@ -1,5 +1,6 @@
 package InClassActivity.ICA05;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,20 +17,23 @@ public class GuessTheNumber {
     do {
       System.out.print("Enter your guess: ");
 
-      if (!scnr.hasNextInt()) {
-        System.out.printf("Error: That's not a valid number. Please enter an integer.%n%n");
-        scnr.next(); // discard invalid input
-
-      } else {
+      try {
         userNum = scnr.nextInt();
 
         if (userNum < 1 || userNum > 10) {
-          System.out.printf("Error: Please guess a number between 1 and 10.%n%n");
+          throw new IllegalArgumentException("Error: Please guess a number between 1 and 10.");
         } else if (userNum == randomNum) {
           System.out.printf("Congratulations! You guessed the right number!%n%n");
         } else {
           System.out.printf("Try again!%n%n");
         }
+
+      } catch (InputMismatchException e) {
+        System.out.printf("Error: That's not a valid number. Please enter an integer.%n%n");
+        scnr.next(); // discard invalid input
+
+      } catch (IllegalArgumentException e) {
+        System.out.printf("%s%n%n", e.getMessage());
       }
 
     } while (userNum != randomNum);
